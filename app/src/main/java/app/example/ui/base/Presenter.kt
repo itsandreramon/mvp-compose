e00e1fba710fa137c1337d32bbb71467b35801b0
itsandreramon <thielea@th-brandenburg.de>
2024-07-14 18:13:51 +0200

    use publish subject for events

--- app/src/main/java/app/example/ui/base/Presenter.kt
+++ app/src/main/java/app/example/ui/base/Presenter.kt
@@ -3,14 +3,14 @@ package app.example.ui.base
 import app.example.ui.screen.event.CounterEvent
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.PublishSubject
 import java.util.concurrent.TimeUnit
 
 abstract class Presenter<S : State<out Event>> {
 
   protected val subscriptions = CompositeDisposable()
 
-  protected val eventSink = BehaviorSubject.create<CounterEvent>()
+  protected val eventSink = PublishSubject.create<CounterEvent>()
 
   abstract val mStateObservable: Observable<S>
 
